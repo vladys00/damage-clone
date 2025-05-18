a34b4c031c1ac9503ee1a90a6deb391e0058af7d
Vladys K <vldk0022@gmail.com>
2025-05-18 14:56:51 +0200

    modified js

--- assets/custom-testimonials-slider.js
+++ assets/custom-testimonials-slider.js
@@ -1,5 +1,7 @@
-const slideshow_elements = document.querySelectorAll(".testimonial-slider");
+const slideshow_elements = document.querySelectorAll(".carousel");
+console.log("script initialized ")
 slideshow_elements.forEach((slideshow_container) => {
+
     const slideSpeed = Number(slideshow_container.dataset.autoplay);
    new Flickity(slideshow_container, {
         cellAlign: "left",
