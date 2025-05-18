64d709a3d97281d8cc6b4ed0c24611f7768ccf45
Vladys K <vldk0022@gmail.com>
2025-05-18 20:10:15 +0200

    gallery slider done

--- assets/custom-gallery-slider.js
+++ assets/custom-gallery-slider.js
@@ -4,10 +4,34 @@ slideshow_elements.forEach((slideshow_container) => {
 
     const slideSpeed = Number(slideshow_container.dataset.autoplay);
    new Flickity(slideshow_container, {
-        cellAlign: "left",
+        cellAlign: 'left', // Changed from center
         wrapAround: true,
         autoPlay: slideSpeed,
-        fullscreen: true,
         pageDots: false,
+        contain: true, // Ensures cells are contained within carousel
+        imagesLoaded: true, // Helps with image sizing
+        prevNextButtons: true,
+        percentPosition: false
     })
+
+    const thumbCarousel = new Flickity('.thumbnail-carousel', {
+    asNavFor: '.main-carousel',
+    cellAlign: 'center',
+    contain: true,
+    pageDots: false,
+    prevNextButtons: false,
+    percentPosition: false,
+    arrowShape: null
+  });
+
+  // Sync carousels
+  mainCarousel.on('select', (index) => {
+    thumbCarousel.select(index);
+  });
+
+  thumbCarousel.on('staticClick', (event, pointer, cellElement, index) => {
+    mainCarousel.select(index);
+  });
 })
+
+
