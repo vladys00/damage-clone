8b0b73e2235e53599a310d50bd48937f7ac7c2d6
Vladys K <vldk0022@gmail.com>
2025-05-20 19:09:54 +0200

    finished stckists dropdown

--- assets/stockists.js
+++ assets/stockists.js
@@ -1,1 +1,10 @@
-console.log("loaded stockists script")
+document.addEventListener('DOMContentLoaded', function() {
+
+  document.querySelectorAll('.country-dropdown .title-container').forEach(title => {
+    title.addEventListener('click', function() {
+      const countryDropdown = this.closest('.country-dropdown');
+      countryDropdown.classList.toggle('active');
+    });
+  });
+  
+});
